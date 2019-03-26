8ef435e7ab7566f6d69e5a8a49ff2ed0ed2f0d10
Kimyen0426 <s2018s01@e-mirim.hs.kr>
2019-03-26 19:46:51 +0900

    add Echo Project

--- 20190326_Echo/src/Echo.java
+++ 20190326_Echo/src/Echo.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Echo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sc = new Scanner(System.in);
+	
+	
+	while(true) {
+		System.out.println("입력하세요>>");
+		String input = sc.nextLine();
+		
+		if(input.equals("exit")) {
+			System.out.println("종료");
+			break;
+		}
+		
+		System.out.println(input);
+	}
+	
+		
 
 	}
 
