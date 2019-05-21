90ab4d4084b28c8806c8b8a1bb34cc543706b4cc
Kimyen0426 <s2018s01@e-mirim.hs.kr>
2019-05-21 19:57:36 +0900

    GUI 가위바위보게임

---
+++ 20190430_GUI/src/images/GUITest6_가위바위보게임.java
@@ -0,0 +1,44 @@
+package images;
+
+import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUITest6_가위바위보게임 {
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame("창이름");
+		JPanel panel = new JPanel();
+		frame.setPreferredSize(new Dimension(1000,800));
+		
+		//Component 만들자
+		JLabel result = new JLabel("Win");
+		ImageIcon computerImage=new ImageIcon("");
+		JLabel computerLabel = new JLabel(computerImage);
+		
+		ImageIcon scissorsImage = new ImageIcon("");
+		JButton scissorsButton = new JButton(scissorsImage);
+		ImageIcon rockImage = new ImageIcon("");
+		JButton rockButton = new JButton(rockImage);
+		ImageIcon paperImage = new ImageIcon("");
+		JButton paperButton = new JButton(paperImage);
+		
+		panel.add(result);
+		panel.add(computerLabel);
+		panel.add(scissorsButton);
+		panel.add(rockButton);
+		panel.add(paperButton);
+		
+		
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
